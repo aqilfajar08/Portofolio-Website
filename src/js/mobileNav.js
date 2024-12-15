5df013faf010bac32ead2a54f2ac63143a7de539
aqilfajar08 <qilzmi08@gmail.com>
2024-12-15 17:50:13 +0700

    first commit

--- src/js/mobileNav.js
+++ src/js/mobileNav.js
@@ -1,15 +1,28 @@
-const burgerButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
-const navbarMenu = document.getElementById('navbar-default');
-const menuButton = document.getElementById("button-menu");
-
-burgerButton.addEventListener('click', () => {
-    if (navbarMenu.classList.contains('hidden')) {
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerButton = document.querySelector('[data-collapse-toggle="navbar-custom"]');
+    const navbarMenu = document.getElementById('navbar-custom');
+  
+    // Mengatur tombol navbar (burger)
+    burgerButton.addEventListener('click', () => {
+      if (navbarMenu.classList.contains('hidden')) {
         navbarMenu.classList.remove('hidden');
         navbarMenu.style.maxHeight = navbarMenu.scrollHeight + 'px';
-    } else {
+      } else {
         navbarMenu.style.maxHeight = '0';
         setTimeout(() => {
-            navbarMenu.classList.add('hidden');
+          navbarMenu.classList.add('hidden');
         }, 500);
-    }
-});
+      }
+    });
+  
+    // Menutup navbar saat link diklik
+    document.querySelectorAll(".nav-link-custom").forEach((link) => {
+      link.addEventListener("click", () => {
+        navbarMenu.style.maxHeight = '0';
+        setTimeout(() => {
+          navbarMenu.classList.add("hidden");
+        }, 500);
+      });
+    });
+  });
+  
